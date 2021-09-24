41ef7af99833e9d2ec7af85b87ca05ad92d03237
Mathias Ewald <mewald@edit.training>
2021-09-24 14:20:12 +0200

    Allow multifile conversion

--- convert-for-hyperdeck.sh
+++ convert-for-hyperdeck.sh
@@ -1,11 +1,27 @@
-IN=$1
-OUT=$2
+FILES=$@
 
-ffmpeg -i $IN \
-   -codec:v prores_ks \
-   -profile:v 4 \
-   -vendor apl0 \
-   -bits_per_mb 8000 \
-   -pix_fmt yuva444p10le \
-   -filter:v fps=25 \
-   $OUT.mov
+for FILE in $FILES; do
+  FULL=$(basename -- "$FILE")
+  DIR=$(dirname -- "$FILE")
+  EXT="${FULL##*.}"
+  NAME="${FULL%.*}"
+  
+  IN=$FULL
+  OUT=$DIR/$NAME.mov
+
+
+  if [ -f $OUT ]; then
+    echo "Skipping $IN. Already exists."
+    continue
+  fi
+
+  echo "Converting $IN => $OUT"
+
+  ffmpeg -i $IN \
+     -codec:v prores_ks \
+     -profile:v 3 \
+     -vendor apl0 \
+     -bits_per_mb 8000 \
+     -filter:v fps=25 \
+     $OUT
+done
